481c7608c3ec2e301b08198487dfb42031e2b886
CEM ŞAHİN <cmshn02@gmail.com>
2022-11-17 15:42:16 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.txtNo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnPSil = new System.Windows.Forms.Button();
+            this.dtPers = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dtPers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(54, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Adı Soyadı:";
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.Location = new System.Drawing.Point(57, 46);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(100, 20);
+            this.txtAdSoyad.TabIndex = 1;
+            // 
+            // txtNo
+            // 
+            this.txtNo.Location = new System.Drawing.Point(57, 111);
+            this.txtNo.Name = "txtNo";
+            this.txtNo.Size = new System.Drawing.Size(100, 20);
+            this.txtNo.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(54, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Numarası:";
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(57, 160);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(109, 23);
+            this.btnKaydet.TabIndex = 4;
+            this.btnKaydet.Text = "Personel Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(57, 189);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(109, 23);
+            this.btnGuncelle.TabIndex = 5;
+            this.btnGuncelle.Text = "Personel Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnPSil
+            // 
+            this.btnPSil.Location = new System.Drawing.Point(57, 218);
+            this.btnPSil.Name = "btnPSil";
+            this.btnPSil.Size = new System.Drawing.Size(109, 23);
+            this.btnPSil.TabIndex = 6;
+            this.btnPSil.Text = "Personel Sil";
+            this.btnPSil.UseVisualStyleBackColor = true;
+            this.btnPSil.Click += new System.EventHandler(this.btnPSil_Click);
+            // 
+            // dtPers
+            // 
+            this.dtPers.AllowUserToAddRows = false;
+            this.dtPers.AllowUserToDeleteRows = false;
+            this.dtPers.BackgroundColor = System.Drawing.SystemColors.ControlDark;
+            this.dtPers.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dtPers.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Raised;
+            this.dtPers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtPers.GridColor = System.Drawing.SystemColors.Info;
+            this.dtPers.Location = new System.Drawing.Point(172, 46);
+            this.dtPers.Name = "dtPers";
+            this.dtPers.ReadOnly = true;
+            this.dtPers.Size = new System.Drawing.Size(415, 195);
+            this.dtPers.TabIndex = 7;
+            this.dtPers.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtPers_CellClick);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1028, 494);
+            this.Controls.Add(this.dtPers);
+            this.Controls.Add(this.btnPSil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.txtNo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtAdSoyad);
+            this.Controls.Add(this.label3);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtPers)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAdiSoyadi;
+        private System.Windows.Forms.TextBox txtNumarasi;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnPersonelKaydet;
+        private System.Windows.Forms.DataGridView DtPersonel;
+        private System.Windows.Forms.Button btnGüncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.TextBox txtNo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnPSil;
+        private System.Windows.Forms.DataGridView dtPers;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Textboxtan gelecek verileri boşaltmak için bir metot yazdım işlemlerden sonra textboxların içi silinsin.
+
+        public void Bosalt()
+        {
+            txtAdSoyad.Text = "";
+            txtNo.Text = "";
+        }
+
+
+        // Veri kaydetme kısmı 
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            //Veri Tabanı Bağlantısı Gerçekleştirdik.
+
+
+            DatabasePersonelEntities db = new DatabasePersonelEntities();
+
+
+            //Veri Ekleme Yapıcağımız class çağrılıyor.
+
+            TBLPersonel tp = new TBLPersonel();
+
+            //Mükerrer numara olmaması için önce textboxttan gelen numara db'de varmı yok mu diye kontrol ediyorum.
+
+           var noKontrol = db.TBLPersonel.FirstOrDefault(x => x.Numarasi == txtNo.Text);
+
+            //Textboxt alanları boş geçilemesin diye bir if bloğu yazdım.
+
+            if (!String.IsNullOrEmpty(txtAdSoyad.Text) && (!String.IsNullOrEmpty(txtNo.Text)))
+            {
+                //Eğer textboxtan gelen numara yok ise 
+
+                if (noKontrol==null)
+                {
+                    // İsim alanı en fazla 50 karakter olacak 
+                    if (txtAdSoyad.Text.Length<=50)
+                    {
+                        tp.AdiSoyadi = txtAdSoyad.Text;
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("İsim soy isim alanı en fazla 50 karakter olabilir.");
+                        return;
+                    }
+
+                    // Numara alanı en fazla 12 karakter olacak
+                    if (txtNo.Text.Length<=12)
+                    {
+                        tp.Numarasi = txtNo.Text;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Numara alanı en fazla 12 karakter olabilir.");
+                        return;
+                    }
+
+
+
+                    
+                    db.TBLPersonel.Add(tp); //Class'ın içerisinde ki verileri Db'ye gönderiyoruz.
+                    db.SaveChanges(); // İşlemi kaydediyoruz.
+
+                    MessageBox.Show("Kayıt Başarılı");
+
+                    Bosalt();
+
+                    // Veri kayıt edildikten son gridwiew içerisine listeleme yapılıyor.
+                    dtPers.DataSource = db.TBLPersonel.ToList();
+
+                }
+
+                else
+                {
+                    MessageBox.Show(" Böyle bir numara mevcut. Lütfen farklı bir numara giriniz");
+                }
+
+
+
+            }
+            else
+            {
+                MessageBox.Show("Lütfen Ad Soyad ve Numara alanlarını boş geçmeyiniz");
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DatabasePersonelEntities db = new DatabasePersonelEntities();
+            dtPers.DataSource = db.TBLPersonel.ToList();
+        }
+
+        int veriId = 0;
+
+        private void dtPers_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //DataGridView içerisinde ki satıra tıkladıgımızda satırın bigisini textbox'lara yazdıracağız.
+
+
+            //Hangi satıra tıklanırsa,1. Sütunun değerini alıp kutulara yazdıracak.
+
+            txtAdSoyad.Text = dtPers.Rows[e.RowIndex].Cells[1].Value.ToString();
+
+            //Hangi satıra tıklanırsa,2. Sütunun değerini alıp kutulara yazdıracak.
+
+            txtNo.Text = dtPers.Rows[e.RowIndex].Cells[2].Value.ToString();
+
+            //Güncelleme yapabilmemiz için,tıklanan veri'nin ID bilgisini almak zorundayız.
+
+            veriId = Convert.ToInt32(dtPers.Rows[e.RowIndex].Cells[0].Value.ToString());
+        }
+
+        //Güncelleme kısmı
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            DatabasePersonelEntities db = new DatabasePersonelEntities();
+
+
+            TBLPersonel tp = db.TBLPersonel.Find(veriId);
+            //Kişiye tıklanmasını istiyorum.
+            if (tp!=null)
+            {
+                //Burda gelen veriler üzerinde bir işlem yapılıp yapılmama kontrolü yapıyorum.
+                if (tp.Numarasi != txtNo.Text || tp.AdiSoyadi != txtAdSoyad.Text)
+                {
+                    tp.AdiSoyadi = txtAdSoyad.Text;
+                    tp.Numarasi = txtNo.Text;
+                    db.SaveChanges();
+                    dtPers.DataSource = db.TBLPersonel.ToList();
+
+                    MessageBox.Show("Veri Güncelleme Başarılı");
+                }
+                else
+                {
+                    MessageBox.Show("Lütfen güncelleme yapılacak alanları değiştirin.");
+                }
+
+                
+            }
+            else
+            {
+                MessageBox.Show("Kişi seçiniz.");
+            }
+           
+          
+
+        }
+
+        private void btnPSil_Click(object sender, EventArgs e)
+        {
+            //İlk olarak alınan Id aresine ait veri daha önce silinmiş mi bakalım 
+
+
+            if (veriId==0)
+            {
+                MessageBox.Show("Veri Bulunamadı");
+
+            }
+
+            else
+            {
+                DatabasePersonelEntities db = new DatabasePersonelEntities();
+                db.TBLPersonel.Remove(db.TBLPersonel.Find(veriId));
+                db.SaveChanges();
+                veriId = 0;
+                Bosalt();
+                dtPers.DataSource = db.TBLPersonel.ToList();
+                MessageBox.Show("Veri silindi.");
+            }
+        }
+    }
+}
